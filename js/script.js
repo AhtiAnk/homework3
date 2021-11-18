5b83968c56caec3b3c4a1a988815b63eb4640412
AhtiAnk <ahtiankro@gmail.com>
2021-11-18 16:49:07 +0200

    initial

--- js/script.js
+++ js/script.js
@@ -24,29 +24,30 @@
       }) 
   }); */
 
-  /*Read from JSON file with server*/
-$(function () {
-        $.get("JSON/obj.json", function(json_obj) { 
-            for (obj of json_obj) { 
-        
-               let div = $('<div class= "flexbox-item">');
-               let head = $('<h3>'); 
-               let time = $('<span>').text(obj.create_time);
-               let box = $('<div class= "imageBox">');
-               let comment = $('<p>').text(obj.post_heading);
+/*Read from JSON file with server*/
+/* $(function () {
+  $.get("JSON/obj.json", function (json_obj) {
+    for (obj of json_obj) {
+      let div = $('<div class= "flexbox-item">');
+      let head = $("<h3>");
+      let time = $("<span>").text(obj.create_time);
+      let box = $('<div class= "imageBox">');
+      let comment = $("<p>").text(obj.post_heading);
 
-        
-               div.append(head); 
-               head.append(time);
-               head.append('<img src="res/images/profile.png">');
-               div.append(box);
-               if (obj.image !== ""){
-                box.append($('<img/>').attr('src',  'data:image/jpg;base64,' + obj.image));
-               }
-               div.append(comment);
-               div.append('<img class = "like" src="res/images/like.png"></img>'); 
-        
-               $('.container-1').append(div) 
-            } 
-        })
-    })
+      div.append(head);
+      head.append(time);
+      head.append('<img src="res/images/profile.png">');
+      div.append(box);
+      if (obj.image !== "") {
+        box.append(
+          $("<img/>").attr("src", "data:image/jpg;base64," + obj.image)
+        );
+      }
+      div.append(comment);
+      div.append('<img class = "like" src="res/images/like.png"></img>');
+
+      $(".container-1").append(div);
+    }
+  });
+});
+ */
